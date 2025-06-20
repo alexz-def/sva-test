c2e29e1a8f51fa76c8144d02bb3f652de36d214d
alexz <alexz73@inbox.ru>
2025-06-20 18:15:28 +0300

    Упаковщик zlib - сторонняя библиотека

---
+++ Packer/Packer.cpp
@@ -0,0 +1,122 @@
+﻿//
+// Packer.cpp
+//
+#include <iostream>
+#include "..\FileQueue.h"
+#include "zlib\zlib.h"
+#include <Windows.h>
+#include <thread>
+#include <filesystem>
+#include <mutex>
+
+bool compress_file(const std::string& input_filename, const std::string& output_filename);
+void compress_wrapper(std::string sfName);
+void thd_func(int thd_id);
+
+std::mutex mtx;
+FileQueue queue;
+
+int main(int argc, char* argv[])
+{
+    std::string folderName;
+
+    if (argc == 1)
+    {
+        std::cout << "Input folder name:\n";
+        std::cin >> folderName;
+    }
+    else
+    {
+        folderName = argv[1];
+    }
+
+    if (!std::filesystem::exists(folderName))
+    {
+        std::cout << folderName << " does not exist!";
+        return 0;
+    }
+    if (!std::filesystem::exists(folderName +"\\_queue.csv"))
+    {
+        std::cout << "Queue file does not exist!";
+        return 0;
+    }
+
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+
+    //std::cout << "NumProc: " << sysinfo.dwNumberOfProcessors << std::endl;
+    std::cout << "NumThds: " << std::thread::hardware_concurrency() << std::endl;
+
+    queue = FileQueue(folderName);
+    queue.Load();
+    std::cout << "Files: " << queue.Count() << std::endl;
+
+    std::thread thd[4];
+
+    //int tCount = 4;
+    int tCount = std::thread::hardware_concurrency();
+
+    for (int i = 1; i <= tCount; i++)
+    {
+        std::cout << "starting thread[" << i << "]...\n";
+        thd[i - 1] = std::thread(thd_func, i);
+    }
+
+    for (int i = 1; i <= tCount; i++)
+    {
+        thd[i-1].join();
+        //std::cout << "thread[" << i << "] finished\n";
+    }
+
+    queue.Remove();
+
+    std::cout << "Completed successfully";
+}
+
+void compress_wrapper(std::string fName)
+{
+    if (std::filesystem::exists(fName))
+    {
+        if (compress_file(fName, fName + ".gz"))
+        {
+            std::cout << "compressed\n";
+        }
+        else
+        {
+            std::cout << "Ooops!\n";
+        }
+    }
+    else
+        std::cout << "File not found!\n";
+}
+
+void thd_func(int thd_id)
+{
+    std::string fn;
+    bool haveItems = false;
+    int cnt = 0;
+
+    do
+    {
+        mtx.lock();
+
+        haveItems = queue.Count() > 0;
+        if (haveItems)
+        {
+            fn = queue.GetFront();
+            queue.PopFront();
+            cnt++;
+
+            std::cout << "thd[" << thd_id << "]: " << fn << "\n";
+        }
+
+        mtx.unlock();
+
+        if (haveItems)
+            compress_wrapper(fn);
+    }
+    while (haveItems);
+
+    if (cnt == 0)
+        std::cout << "thd[" << thd_id << "]: queue is empty\n";
+}
---
+++ Packer/zlib_compress.cpp
@@ -0,0 +1,84 @@
+
+#include <iostream>
+#include <fstream>
+#include "zlib\zutil.h"
+
+bool compress_file(const std::string& input_filename, const std::string& output_filename) 
+{
+    const size_t CHUNK_SIZE = 16384;
+
+    std::ifstream input_file(input_filename, std::ios::binary);
+    std::ofstream output_file(output_filename, std::ios::binary);
+
+    if (!input_file.is_open() || !output_file.is_open())
+    {
+        std::cerr << "File opening error!" << std::endl;
+        return false;
+    }
+
+    // Инициализация zlib
+    z_stream strm;
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+
+    if (deflateInit(&strm, Z_DEFAULT_COMPRESSION) != Z_OK)
+    {
+        std::cerr << "Deflate initialization error!" << std::endl;
+        return false;
+    }
+
+    // Буферы для чтения и записи
+    char input_buffer[CHUNK_SIZE];
+    char output_buffer[CHUNK_SIZE];
+
+    strm.next_out = reinterpret_cast<Bytef*>(output_buffer);
+    strm.avail_out = CHUNK_SIZE;
+
+    int flush = Z_NO_FLUSH;
+    int ret;
+
+    do
+    {
+        input_file.read(input_buffer, CHUNK_SIZE);
+
+        if (input_file.eof())
+            flush = Z_FINISH;
+
+        strm.avail_in = input_file.gcount();
+        strm.next_in = reinterpret_cast<Bytef*>(input_buffer);
+
+        do
+        {
+            ret = deflate(&strm, flush);
+
+            if (ret == Z_STREAM_ERROR)
+            {
+                std::cerr << "Compressing error!" << std::endl;
+                deflateEnd(&strm);
+                return false;
+            }
+            if (strm.avail_out == 0)
+            {
+                output_file.write(output_buffer, CHUNK_SIZE);
+                strm.next_out = reinterpret_cast<Bytef*>(output_buffer);
+                strm.avail_out = CHUNK_SIZE;
+            }
+
+        } while (strm.avail_in > 0);    // || flush == Z_FINISH
+
+    } while (!input_file.eof());
+
+    // Запись оставшихся данных
+    if (strm.avail_out < CHUNK_SIZE)
+    {
+        output_file.write(output_buffer, CHUNK_SIZE - strm.avail_out);
+    }
+
+    deflateEnd(&strm);
+
+    input_file.close();
+    output_file.close();
+
+    return true;
+}
